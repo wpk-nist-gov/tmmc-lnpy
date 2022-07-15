f10947e5d6d82b36a6cf3b458973afc65a918bae
wpk <william.krekelberg@nist.gov>
2022-07-15 10:01:35 -0400

    update versioning

--- lnPi/__init__.py
+++ lnPi/__init__.py
@@ -9,3 +9,16 @@ from . import xlnPi
 
 from .utils import dim_to_suffix
 from .options import set_options, OPTIONS
+
+
+
+try:
+    import pkg_resources
+    __version__ = pkg_resources.get_distribution("cmomy").version
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "999"
+
+__author__ = """William P. Krekelberg"""
+__email__ = "wpk@nist.gov"
