31fa0b57e875a84b538cbf64859c30d5239eb9a6
wpk <william.krekelberg@nist.gov>
2023-06-30 09:31:46 -0400

    chore: removed unused stuff

--- src/lnpy/__init__.py
+++ src/lnpy/__init__.py
@@ -1,26 +1,9 @@
 """Publicly accessible classes/routines."""
-# from .ensembles import xCanonical, xGrandCanonical
-# from .lnpidata import MaskedlnPiDelayed, lnPiArray, lnPiMasked
 from . import ensembles, lnpienergy, segment
 from .lnpidata import lnPiMasked
-
-# from .lnpienergy import (
-#     merge_regions,
-#     wFreeEnergy,
-#     wFreeEnergyCollection,
-#     wFreeEnergyPhases,
-# )
 from .lnpiseries import lnPiCollection
-
-# from .maskedlnpi_legacy import MaskedlnPiLegacy
 from .options import OPTIONS, set_options
 from .segment import PhaseCreator
-
-# from .stability import Binodals, Spinodals
-# from .utils import dim_to_suffix
-
-# Legacy stuff
-
 
 # updated versioning scheme
 try:
@@ -36,28 +19,12 @@ __all__ = [
     "__author__",
     "__email__",
     "__version__",
-    # "lnPiArray",
     "lnPiMasked",
     "lnPiCollection",
     "segment",
     "ensembles",
     "lnpienergy",
-    # "MaskedlnPiDelayed",
-    # "MaskedlnPiLegacy",
-    # "xGrandCanonical",
-    # "xCanonical",
-    # "wFreeEnergyPhases",
-    # "wFreeEnergyCollection",
-    # "wFreeEnergy",
-    # "merge_regions",
-    # "Segmenter",
     "PhaseCreator",
-    # "peak_local_max_adaptive",
-    # "BuildPhases_mu",
-    # "BuildPhases_dmu",
-    # "dim_to_suffix",
     "set_options",
     "OPTIONS",
-    # "Spinodals",
-    # "Binodals",
 ]
